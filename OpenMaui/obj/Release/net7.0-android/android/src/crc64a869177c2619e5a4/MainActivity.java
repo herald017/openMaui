a417fecdd12674147304cb20eb48f578a5d6069c
Herald Ray Reuta <h.reuta@psei.ph>
2023-07-07 17:09:59 +0800

    commit signing setup

---
+++ OpenMaui/obj/Release/net7.0-android/android/src/crc64a869177c2619e5a4/MainActivity.java
@@ -0,0 +1,48 @@
+package crc64a869177c2619e5a4;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("OpenMaui.MainActivity, OpenMaui", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("OpenMaui.MainActivity, OpenMaui", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("OpenMaui.MainActivity, OpenMaui", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
